a7323988fe09184aa44c957a57e4138b189a0909
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-08 18:49:02 +0100

    allitolag mukodik

--- Menhely/src/app/input/input.component.ts
+++ Menhely/src/app/input/input.component.ts
@@ -1,43 +1,36 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
-export class InputComponent {
-  formData = {
-    nev: '',
-    eletkor: null,
-    fajta: '',
-    szin: '',
-    nem: 0,
-    chip: '',
-    faj: '',
-    veszettseg: 0,
-    parvo: 0,
-    fereghajto: 0,
-    datum: ''
-  };
+export class InputComponent{
+
+  nev = (document.getElementById('name') as HTMLInputElement).value;
+  eletkor = (document.getElementById('age') as HTMLInputElement).value;
+  nem = (document.getElementById('gender') as HTMLSelectElement).value;
+  faj = (document.getElementById('species') as HTMLInputElement).value;
+  fajta = (document.getElementById('breed') as HTMLInputElement).value;
+  chip = (document.getElementById('chip') as HTMLInputElement).value;
+  szin = (document.getElementById('color') as HTMLInputElement).value;
+  veszettseg = (document.getElementById('rabies') as HTMLInputElement).checked ? 1 : 0;
+  parvo = (document.getElementById('parvo') as HTMLInputElement).checked ? 1 : 0;
+  fereghajto = (document.getElementById('deworming') as HTMLInputElement).checked ? 1 : 0;
+  datum = (document.getElementById('date') as HTMLInputElement).value;
 
   constructor(private http: HttpClient) {}
 
   onSubmit() {
-    const { nev, eletkor, fajta, szin, nem, chip, faj, veszettseg, parvo, fereghajto, datum } = this.formData;
 
-    // Ellenőrzés: ha a datum nincs megadva, figyelmeztetést adhatsz
-    if (!datum) {
-      alert('A dátum megadása kötelező!');
-      return;  // Megakadályozza a továbbhaladást, ha nincs dátum
-    }
-
-    // API URL kialakítása
-    const url = `https://berenandor.moriczcloud.hu/menhely/feltoltes/${nev}/${eletkor}/${fajta}/${szin}/${nem}/${chip}/${faj}/${veszettseg}/${parvo}/${fereghajto}/${datum}`;
+    
+    const url = `https://berenandor.moriczcloud.hu/menhely/feltoltes/`+this.nev+`/`+this.eletkor+`/`+this.fajta+`/`+this.szin+`/`+this.nem+`/`+this.chip+`/`+this.faj+`/`+this.veszettseg+`/`+this.parvo+`/`+this.fereghajto+`/`+this.datum;
     
     this.http.get(url).subscribe(response => {
       console.log('Response:', response);
