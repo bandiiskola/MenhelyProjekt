344d4231ec1f58f1a1e8cfb2e43b73c67401dde1
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-08 18:52:00 +0100

    mukodik

--- Menhely/src/app/input/input.component.ts
+++ Menhely/src/app/input/input.component.ts
@@ -13,24 +13,25 @@ import { RouterModule } from '@angular/router';
 })
 export class InputComponent{
 
-  nev = (document.getElementById('name') as HTMLInputElement).value;
-  eletkor = (document.getElementById('age') as HTMLInputElement).value;
-  nem = (document.getElementById('gender') as HTMLSelectElement).value;
-  faj = (document.getElementById('species') as HTMLInputElement).value;
-  fajta = (document.getElementById('breed') as HTMLInputElement).value;
-  chip = (document.getElementById('chip') as HTMLInputElement).value;
-  szin = (document.getElementById('color') as HTMLInputElement).value;
-  veszettseg = (document.getElementById('rabies') as HTMLInputElement).checked ? 1 : 0;
-  parvo = (document.getElementById('parvo') as HTMLInputElement).checked ? 1 : 0;
-  fereghajto = (document.getElementById('deworming') as HTMLInputElement).checked ? 1 : 0;
-  datum = (document.getElementById('date') as HTMLInputElement).value;
+  
 
   constructor(private http: HttpClient) {}
 
   onSubmit() {
 
+    const nev = (document.getElementById('name') as HTMLInputElement).value;
+    const eletkor = (document.getElementById('age') as HTMLInputElement).value;
+    const nem = (document.getElementById('gender') as HTMLSelectElement).value;
+    const faj = (document.getElementById('species') as HTMLInputElement).value;
+    const fajta = (document.getElementById('breed') as HTMLInputElement).value;
+    const chip = (document.getElementById('chip') as HTMLInputElement).value;
+    const szin = (document.getElementById('color') as HTMLInputElement).value;
+    const veszettseg = (document.getElementById('rabies') as HTMLInputElement).checked ? 1 : 0;
+    const parvo = (document.getElementById('parvo') as HTMLInputElement).checked ? 1 : 0;
+    const fereghajto = (document.getElementById('deworming') as HTMLInputElement).checked ? 1 : 0;
+    const datum = (document.getElementById('date') as HTMLInputElement).value;
     
-    const url = `https://berenandor.moriczcloud.hu/menhely/feltoltes/`+this.nev+`/`+this.eletkor+`/`+this.fajta+`/`+this.szin+`/`+this.nem+`/`+this.chip+`/`+this.faj+`/`+this.veszettseg+`/`+this.parvo+`/`+this.fereghajto+`/`+this.datum;
+    const url = `https://berenandor.moriczcloud.hu/menhely/feltoltes/`+nev+`/`+eletkor+`/`+fajta+`/`+szin+`/`+nem+`/`+chip+`/`+faj+`/`+veszettseg+`/`+parvo+`/`+fereghajto+`/`+datum;
     
     this.http.get(url).subscribe(response => {
       console.log('Response:', response);
