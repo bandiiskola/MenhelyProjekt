50a7ee749c040254282c796c6eed213dccb77212
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-08 17:11:47 +0100

    Alap működés

--- Menhely/src/app/input/input.component.ts
+++ Menhely/src/app/input/input.component.ts
@@ -1,10 +1,55 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, HttpClient],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
-export class InputComponent {}
+export class InputComponent {
+  formData = {
+    nev: '',
+    eletkor: null,
+    fajta: '',
+    szin: '',
+    nem: 0,
+    chip: '',
+    faj: '',
+    veszettseg: 0,
+    parvo: 0,
+    fereghajto: 0,
+    datum: '',
+  };
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const {
+      nev,
+      eletkor,
+      fajta,
+      szin,
+      nem,
+      chip,
+      faj,
+      veszettseg,
+      parvo,
+      fereghajto,
+      datum,
+    } = this.formData;
+    const url = `https://berenandor.moriczcloud.hu/menhely/feltoltes/${nev}/${eletkor}/${fajta}/${szin}/${nem}/${chip}/${faj}/${veszettseg}/${parvo}/${fereghajto}/${datum}`;
+
+    this.http.get(url).subscribe(
+      (response) => {
+        console.log('Response:', response);
+        alert('Adatok sikeresen feltöltve!');
+      },
+      (error) => {
+        console.error('Error:', error);
+        alert('Hiba történt a feltöltés során.');
+      }
+    );
+  }
+}
