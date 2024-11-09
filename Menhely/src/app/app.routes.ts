ac31a4e8b4cb8adbae8cd6cac87d0faa7b7d6e67
Meszarosistvan0828 <meszarosistvan0828@gmail.com>
2024-11-09 22:20:56 +0100

    css és fooldal

--- Menhely/src/app/app.routes.ts
+++ Menhely/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListaComponent } from './lista/lista.component';
 import { InputComponent } from './input/input.component';
+import { FooldalComponent } from './fooldal/fooldal.component';
 
 export const routes: Routes = [
-    {path:'', component: ListaComponent},
+    {path:'lista', component: ListaComponent},
     {path: 'feltoltes', component: InputComponent},
     {path: 'modosit/:id', component: InputComponent },
+    {path:'', component: FooldalComponent},
 ];
