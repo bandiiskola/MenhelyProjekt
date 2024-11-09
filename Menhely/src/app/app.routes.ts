d5d38dd71ec87f28bf36a5912b2a17b489cd1510
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-09 13:35:37 +0100

    prototipus

--- Menhely/src/app/app.routes.ts
+++ Menhely/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { ListaComponent } from './lista/lista.component';
 
 export const routes: Routes = [
-    {path:'lista', component: ListaComponent}
+    {path:'', component: ListaComponent}
 ];
