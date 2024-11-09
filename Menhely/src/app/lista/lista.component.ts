320f7eaa9c66ff59912df2796ecaa8f7e30e2812
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-09 11:54:36 +0100

    stílus nélkül betőlt kész

--- Menhely/src/app/lista/lista.component.ts
+++ Menhely/src/app/lista/lista.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -6,10 +7,19 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [HttpClientModule, RouterModule],
+  imports: [HttpClientModule, RouterModule, CommonModule],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
 export class ListaComponent implements OnInit{
+  pets: any[] = [];
   constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const url = `https://berenandor.moriczcloud.hu/menhely/listazas`;
+    this.http.get<any[]>(url).subscribe((data: any[]) => {
+      this.pets = data;
+      console.log('Adatok betöltve:', this.pets);
+    });
+  }
 }
